b048c0e7ef2856f694083c28b3d340075e2ec30e
Guest User <Guest@epicodus-9.local>
2018-08-03 15:18:17 -0700

    Add test for ages in seconds

---
+++ spec/ageCalc-spec.js
@@ -0,0 +1,14 @@
+import { AgeCalc } from './../src/ageCalc.js';
+
+describe('AgeCalc', function() {
+  it('should count seconds from a date until todays date', function() {
+    let newage = new AgeCalc(27);
+    newage.ageInSeconds();
+    expect(851472000).toEqual(newage.age);
+  });
+  it('should return users age on mercury', function() {
+    let newage = new AgeCalc(27);
+    newage.ageMercury();
+    expect(6.4799999999999995).toEqual(newage.age);
+  });
+});
--- src/AgeCalc.js
+++ src/AgeCalc.js
@@ -1,5 +1,27 @@
-//business logic
-
-// export function journal(goal) {
-//
-// }
+export class AgeCalc {
+  constructor(age){
+    this.age = age;
+  }
+  ageInSeconds(){
+    this.age = this.age * 31536000;
+    // console.log(seconds);
+    // seconds * 31536000;
+    // // let d = new Date(2018, 8, 3, 10, 10, 10);
+    // // answer = d - l;
+    // console.log(60 * 60 * 24 * 365 * 27);
+     console.log(this.age);
+    // return seconds;
+  }
+  ageMercury(){
+    this.age = this.age * .24;
+  }
+  ageVenus(){
+    this.age = this.age * .62;
+  }
+  ageMars(){
+    this.age = this.age * 1.88;
+  }
+  ageJupiter(){
+    this.age = this.age * 11.86;
+  }
+}
